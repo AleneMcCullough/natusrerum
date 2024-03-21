f5ee03d98ebe6c7bcf47a8fd09c473099347c1fe
AleneMcCullough <andreamanleyb98246820475@outlook.com>
2024-03-21 19:56:01 +0800

    hic

---
+++ tests/test-cssfn-react/src/stylesheet8_css.ts
@@ -0,0 +1,9 @@
+import {
+    dynamicStyleSheet,
+} from '@cssfn/cssfn-react'
+
+
+
+export const useStyleSheet8 = dynamicStyleSheet(
+    () => import(/* webpackChunkName: "stylesheet8_css_dyn" */ './stylesheet8_css_dyn')
+, { id: 'styleSheet-8'});
